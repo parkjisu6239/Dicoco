0c94dc2e31a81a20968791b22555542fd47364ff
parkjisu <parkjisu6239@gmail.com>
2022-02-15 12:41:55 +0900

    feat: Add valid rule
    세션은 20자 이내의 영어만
    이름은 20자 이내의 영어, 한국어, 숫자,공백만 포함 가능

--- src/components/Home/SetName.js
+++ src/components/Home/SetName.js
@@ -4,8 +4,21 @@ import style from './SetName.module.css'
 function SetName({setName}) {
     const anonymous = require('./anonymous.json')
     const [input, setInput] = useState('익명의 ' + anonymous[Math.floor(Math.random() * anonymous.length)])
+    const [valid, setValid] = useState(true)
+    const [validMsg, setValidMsg] = useState('')
+    const reg = /^[a-zA-Zㄱ-ㅎ가-힣0-9\s]+$/
 
     function ChangeInput(e) {
+        if (!reg.test(e.target.value)) {
+            setValid(false)
+            setValidMsg('Only ko, eng, num. up to 20 characters.')
+        } else {
+            setValid(true)
+            setValidMsg('')
+        }
+
+        if (e.target.value.length > 20) return
+
         setInput(e.target.value);
     }
 
@@ -27,11 +40,12 @@ function SetName({setName}) {
                         required
                     />
                     <button 
-                        disabled={input.trim() ? false : true}
+                        disabled={valid ? false : true}
                         onClick={submitInput}>
                         →
                     </button>
                 </div>
+                <p className={style.warn}>{validMsg}</p>
             </form>
         </div>
     )
