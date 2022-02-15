bfa361e071a5cbae00901819aadee0ddd000d882
parkjisu <parkjisu6239@gmail.com>
2022-02-15 16:28:21 +0900

    feat: Toggle Participants list
    토글 버튼 추가, 스타일 시트 분리 및 추가

--- src/components/ChatRoom/Participants.js
+++ src/components/ChatRoom/Participants.js
@@ -1,9 +1,9 @@
 import style from './Participants.module.css'
 import { BsFillPeopleFill } from 'react-icons/bs' 
 
-function Participants({participants, pk}) {
+function Participants({participantsToggle, participants, pk}) {
     return (
-        <div className={style.participants}>
+        <div className={`${style.participants} ${participantsToggle ? style.toggleOn : style.toggleOff}`}>
             <h3>chatty <BsFillPeopleFill/> {participants.length}</h3>
             <ul>
                 {participants.map(participant => 
--- src/components/ChatRoom/RoomHeader.js
+++ src/components/ChatRoom/RoomHeader.js
@@ -1,16 +1,24 @@
 import { Link } from 'react-router-dom' 
 import style from './RoomHeader.module.css'
 import { BiExit } from 'react-icons/bi'
+import { BsFillPeopleFill } from 'react-icons/bs' 
 
-function RoomHeader({sessionId}) {
+function RoomHeader({sessionId, participantsToggle, toggleParticipants}) {
     return (
         <header className={style.top}>
             <h2>{sessionId}</h2>
-            <Link to="/">
-                <BiExit 
-                    className={style.exit}
-                    size="1.3rem"/>
-            </Link>
+            <div className={style.icons}>
+                <div 
+                    className={`${style.participants} ${participantsToggle ? style.toggleOn : style.toggleOff}`}
+                    onClick={toggleParticipants}>
+                        <BsFillPeopleFill size="1.3rem"/>
+                </div>
+                <Link to="/">
+                    <BiExit 
+                        className={style.exit}
+                        size="1.3rem"/>
+                </Link>
+            </div>
         </header>
     )
 }
