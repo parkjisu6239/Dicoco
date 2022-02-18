89743b13262ba931b270602a7ef52eb50da114eb
parkjisu6239 <parkjisu6239@gmail.com>
2022-02-18 21:00:23 +0900

    feat: Add camera icon

--- src/components/ChatRoom/RoomHeader.js
+++ src/components/ChatRoom/RoomHeader.js
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom' 
 import style from './RoomHeader.module.css'
 import { BiExit } from 'react-icons/bi'
-import { BsFillPeopleFill } from 'react-icons/bs' 
+import { BsFillPeopleFill } from 'react-icons/bs'
+import { IoIosVideocam } from 'react-icons/io' 
 
-function RoomHeader({sessionId, participantsToggle, toggleParticipants}) {
+function RoomHeader({sessionId, participantsToggle, toggleParticipants, cameraOn, toggleCameraOn}) {
     return (
         <header className={style.top}>
             <h2>{sessionId}</h2>
             <div className={style.icons}>
+                <div 
+                    className={`${style.camera} ${cameraOn ? style.cameraOn : style.cameraOff}`}
+                    onClick={toggleCameraOn}>
+                        <IoIosVideocam size="1.3rem"/>
+                </div>
                 <div 
                     className={`${style.participants} ${participantsToggle ? style.toggleOn : style.toggleOff}`}
                     onClick={toggleParticipants}>
